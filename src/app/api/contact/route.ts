97c1e63ecee80e5a28e6865193f2e821df16ee1e
diegovfeder <63731464+diegovfeder@users.noreply.github.com>
2024-12-12 12:26:21 -0300

    fix: imports resend api key inside POST contact route

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -2,9 +2,9 @@ import { Resend } from "resend";
 import { NextResponse } from "next/server";
 import { ContactEmailTemplate } from "@/components/email-template";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(request: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
   try {
     const body = await request.json();
     const { name, email, message } = body;
